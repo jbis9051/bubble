89d24489950470d7bf1e5b2ba32e09378c469316
clef98 <39439948+clef98@users.noreply.github.com>
2022-06-02 23:23:48 -0400

    initial up.sql and down.sql (#6)
    * initial up.sql and down.sql
    
    * adjusted attachment FK in message to be null, because a message does not need an attachment
    
    Co-authored-by: ericsong <ericsong@umich.edu>

--- packages/rust/server/migrations/20220528030155_initial.down.sql
+++ packages/rust/server/migrations/20220528030155_initial.down.sql
@@ -1,1 +1,10 @@
--- Add down migration script here
+DROP TABLE message;
+DROP TABLE user_group;
+DROP TABLE role;
+DROP TABLE location_group;
+DROP TABLE location;
+DROP TABLE "group";
+DROP TABLE "user";
+DROP TABLE attachment;
+
+
--- packages/rust/server/migrations/20220528030155_initial.up.sql
+++ packages/rust/server/migrations/20220528030155_initial.up.sql
@@ -1,1 +1,72 @@
--- Add up migration script here
+CREATE TABLE "user"
+(
+    id              serial PRIMARY KEY,
+    username        VARCHAR(255) UNIQUE NOT NULL,
+    password        VARCHAR(255)        NOT NULL,
+    profile_picture VARCHAR(255)        NULL,
+    email           VARCHAR(255) UNIQUE NULL,
+    phone           VARCHAR(11) UNIQUE  NOT NULL,
+    name            VARCHAR(255)        NOT NULL,
+    created         TIMESTAMP           NOT NULL
+);
+
+CREATE TABLE location
+(
+    id        serial PRIMARY KEY,
+    user_id   INT REFERENCES "user" (id) NOT NULL,
+    latitude  FLOAT(10)                  NOT NULL,
+    longitude FLOAT(10)                  NOT NULL,
+    created   TIMESTAMP                  NOT NULL
+);
+
+CREATE TABLE "group"
+(
+    id         serial PRIMARY KEY,
+    group_name VARCHAR(255) NOT NULL,
+    created    TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE location_group
+(
+    id          serial PRIMARY KEY,
+    location_id INT REFERENCES location (id) NOT NULL,
+    group_id    INT REFERENCES "group" (id)  NOT NULL,
+    created     TIMESTAMP                    NOT NULL
+
+);
+
+CREATE TABLE role
+(
+    id      serial PRIMARY KEY,
+    role    VARCHAR(255) NOT NULL,
+    created TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE attachment
+(
+    id      serial PRIMARY KEY,
+    "file"  VARCHAR(255) NOT NULL,
+    created TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE user_group
+(
+    id       serial PRIMARY KEY,
+    user_id  INT REFERENCES "user" (id)  NOT NULL,
+    group_id INT REFERENCES "group" (id) NOT NULL,
+    role_id  INT REFERENCES role (id)    NOT NULL,
+    created  TIMESTAMP                   NOT NULL
+);
+
+
+CREATE TABLE message
+(
+    id            serial PRIMARY KEY,
+    attachment_id INT REFERENCES attachment (id) NULL,
+    group_id      INT REFERENCES "group" (id)    NOT NULL,
+    user_id       INT REFERENCES "user" (id)     NOT NULL,
+    content       VARCHAR(255)                   NULL,
+    created       TIMESTAMP                      NOT NULL
+);
+
+
