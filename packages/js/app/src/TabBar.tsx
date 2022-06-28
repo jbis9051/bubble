913136d55939fe8d33433b406f89e8e16cd99665
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-06-28 09:07:41 -0400

    Add more views, update tab bar

--- packages/js/app/src/TabBar.tsx
+++ packages/js/app/src/TabBar.tsx
@@ -3,6 +3,9 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './views/HomeScreen';
 import SettingsScreen from './views/SettingsScreen';
+import MapScreen from './views/MapScreen';
+import ProfileScreen from './views/ProfileScreen';
+import FriendsScreen from './views/FriendsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +14,9 @@ const TabBar = () => (
         initialRouteName='Home'
     >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Friends" component={FriendsScreen} />
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
 );
 
---
+++ packages/js/app/src/views/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ProfileScreen = () => (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Profile</Text>
+    </View>
+);
+
+export default ProfileScreen;
