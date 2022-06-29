74c6a7628eb840c2aefd84d2f3f5373490724b69
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-06-29 13:05:52 -0400

    Create tab-bar with icons

--- packages/js/app/src/TabBar.tsx
+++ packages/js/app/src/TabBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './views/HomeScreen';
@@ -9,14 +10,119 @@ import FriendsScreen from './views/FriendsScreen';
 
 const Tab = createBottomTabNavigator();
 
+
+
 const TabBar = () => (
     <Tab.Navigator
+        screenOptions={{
+            tabBarShowLabel: false,
+            tabBarStyle: {
+                backgroundColor: '#ffffff',
+                height: 90,
+            }
+        }}
         initialRouteName='Home'
     >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Friends" component={FriendsScreen} />
-        <Tab.Screen name="Map" component={MapScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Home" 
+            component={HomeScreen} 
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <View style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        top: 10
+                    }}>
+                        <Image
+                            source={require('../assets/icons/home.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 35,
+                                height: 35,
+                                tintColor: focused ? '#e32f45' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
+                            Home
+                        </Text>
+                    </View>
+                )
+            }}
+        />
+        <Tab.Screen name="Friends" 
+            component={FriendsScreen}
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <View style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        top: 10
+                    }}>
+                        <Image
+                            source={require('../assets/icons/friends.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 35,
+                                height: 35,
+                                tintColor: focused ? '#e32f45' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
+                            Friends
+                        </Text>
+                    </View>
+                )
+            }} 
+        />
+        <Tab.Screen name="Map" 
+            component={MapScreen} 
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <View style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        top: 10
+                    }}>
+                        <Image
+                            source={require('../assets/icons/globe.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: focused ? '#e32f45' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
+                            Map
+                        </Text>
+                    </View>
+                )
+            }} 
+        />
+        <Tab.Screen name="Profile" 
+            component={ProfileScreen} 
+            options={{
+                tabBarIcon: ({focused}) => (
+                    <View style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        top: 10
+                    }}>
+                        <Image
+                            source={require('../assets/icons/user.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: focused ? '#e32f45' : '#748c94'
+                            }}
+                        />
+                        <Text style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12}}>
+                            User
+                        </Text>
+                    </View>
+                )
+            }} 
+        />
     </Tab.Navigator>
 );
 
