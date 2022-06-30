c89c5671fb7eea95f10ede7d637662d308247671
ericsong <ericysong007@gmail.com>
2022-06-29 22:47:06 -0400

    Cargo setup, imports, structures and params syntax issues

--- packages/rust/server/src/routes/group.rs
+++ packages/rust/server/src/routes/group.rs
@@ -1,10 +1,12 @@
+use axum::extract::Path;
 use axum::routing::get;
 use axum::handler::Handler;
+use axum::Router;
 
-#[tokio::main]
-async fn main() {
+pub fn router() -> Router {
     let app = Router::new()
         .route("/group/:name", get(create))
+        //have to make unique
         .route("/group/:id", get(read))
         .route("/group/:id", get(read_users))
         .route("/group/:id/:new_users", get(add_users))
@@ -14,40 +16,40 @@ async fn main() {
 }
 
 async fn create(Path(params): &str){
-    let name = params.get(name);
+    let name = params.get("name");
     todo!();
 }
 
 async fn read(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     todo!();
 }
 
 // unsure about multiple inputs
 async fn read_users(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     todo!();
 }
 
 async fn add_users(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     let new_users: Vec<&str> = params.get(add_users);
     todo!();
 }
 
 async fn delete_users(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     let users_to_delete: Vec<&str> = params.get(users_to_delete);
     todo!();
 }
 
 async fn change_name(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     let name = params.get(name);
     todo!();
 }
 
 async fn delete(Path(params): &str){
-    let group_id = params.get(id);
+    let group_id = params.get("id");
     todo!();
 }
