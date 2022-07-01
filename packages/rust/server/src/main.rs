377b46a215a10ea676a073f7dded4141b86f815d
clef98 <ericysong007@gmail.com>
2022-06-30 23:07:16 -0400

    template for sql pool and model functions

--- packages/rust/server/src/main.rs
+++ packages/rust/server/src/main.rs
@@ -1,7 +1,10 @@
 mod routes;
 mod models;
-
+use sqlx::postgres::PgPoolOptions;
 
 fn main() {
     println!("Hello, world!");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost/test").await?;
 }
--- packages/rust/server/src/models/group.rs
+++ packages/rust/server/src/models/group.rs
@@ -10,7 +10,7 @@ struct Group{
 
 // CRUD functions
 impl Group{
-    fn create(&self, name: String) -> Group{
+    fn create(&self, name: String) -> Group {
        let group = Group {
            id: "".to_string(),
            uuid: "".to_string(),
@@ -20,19 +20,21 @@ impl Group{
        };
         group
     }
-    fn read(&self){
+    fn read(&mut self){
         todo!();
     }
     fn add_users(&mut self, mut new_users: Vec<String>){
-        self.members.append(&mut new_users);
+        *self.members.append(&mut new_users);
     }
-    fn delete_users(&self, users_to_delete: Vec<String>){
+    fn delete_users(&mut self, users_to_delete: Vec<String>){
         for i in users_to_delete{
-            if &self.members.has
+            if &self.members.contains(&i){
+                &self.members.retain(|x| *x != &i);
+            }
         }
     }
     fn change_name(&self, name: String){
-        todo!();
+        &self.group_name = &name;
     }
     fn delete_group(&self){
         todo!();
