5067c0043e82beaa92d8ae286df288f86ac7366a
Starfish <67656192+SeaUrc@users.noreply.github.com>
2022-07-04 18:01:16 -0400

    Upload Login and Splash files

---
+++ packages/js/app/src/views/LoginScreen.tsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import {ImageBackground, Text, View, StyleSheet, Button, StatusBar, TouchableOpacity} from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import Header from '../components/Header';
+import TextInputBox from "../components/TextInputBox";
+import colors from '../constants/Colors';
+
+type RootStackParamList = {
+    Login: undefined,
+    Signup1: undefined,
+    Signup2: undefined,
+    Splash: undefined,
+};
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: colors.white,
+        width: '100%',
+        height: '100%',
+    },
+    backgroundImage:{
+        alignItems: 'center',
+    },
+    title: {
+        flex: 0.7,
+        alignItems: 'center',
+        fontSize: 45,
+        fontWeight: '100',
+    },
+    textContainer:{
+        flex: 3,
+        justifyContent: 'center',
+    },
+    textInput: {
+        borderTopColor: colors.white,
+        borderRightColor: colors.white,
+        borderLeftColor: colors.white,
+        height: 50,
+        width: 300,
+        margin: 7,
+        borderWidth: 1,
+    },textInputDescriptors:{
+        color: colors.darkGrey,
+    },loginContainer: {
+        flex: 5,
+    },
+    login:{
+        height: 40,
+        width: 200,
+        margin: 7,
+        borderWidth: 1,
+        borderRadius: 15,
+        padding: 10,
+        alignItems:'center',
+        justifyContent:'center',
+    },forgot:{
+        fontWeight: '200',
+        fontSize: 13,
+        textAlign: 'center',
+    }
+})
+
+function Login({route, navigation}: Props) {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('../assets/background.png')}
+                style={styles.backgroundImage}
+            >
+                <Header page={'Splash'}/>
+                <Text style={styles.title}>Welcome back</Text>
+                <View style={styles.textContainer}>
+                    <TextInputBox
+                        descriptor='Username'
+                        required={false}
+                    />
+                    <TextInputBox
+                        descriptor='Password'
+                        required={false}
+                    />
+                </View>
+                <View style={styles.loginContainer}>
+                    <TouchableOpacity style={styles.login}>
+                        <Text>Log In</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.forgot} onPress={() => {navigation.navigate('')}}>Forgot password?</Text>
+                </View>
+            </ImageBackground>
+        </View>
+    );
+}
+export default Login;
---
+++ packages/js/app/src/views/SplashScreen.tsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {Text, View, StyleSheet, ImageBackground, TouchableOpacity} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import colors from '../constants/Colors';
+
+type RootStackParamList = {
+    Login: undefined,
+    Signup1: undefined,
+    Signup2: undefined,
+    Splash: undefined,
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>;
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: colors.white,
+        width: '100%',
+        height: '100%',
+    },backgroundImage:{
+        height: '100%',
+        width: '100%',
+    },
+    title: {
+        fontSize: 95,
+        fontWeight: '100',
+    },
+    titleContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }, info:{
+        flex: 1,
+        textAlign: 'center',
+    },buttonsContainer:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttons:{
+            height: 50,
+            width: 250,
+            margin: 7,
+            borderWidth: 1,
+            borderRadius: 15,
+            padding: 10,
+            alignItems:'center',
+            justifyContent:'center',
+    },
+})
+
+function Splash({route, navigation}: Props) {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('../assets/background.png')}
+                style={styles.backgroundImage}
+            >
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Bubble</Text>
+                </View>
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity
+                        style={styles.buttons}
+                        onPress={() => {navigation.navigate('Signup1')}}>
+                        <Text>Sign up</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={styles.buttons}
+                        onPress={() => {navigation.navigate('Login')}}>
+                        <Text>Log in</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </View>
+    );
+}
+export default Splash;
