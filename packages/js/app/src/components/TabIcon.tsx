4731928836e539be0eeeb774bd3325659e795d20
Josh Brown <josh9051@gmail.com>
2022-07-04 18:09:21 -0400

    Feature/logo (#10)
    * adds some logos
    
    * adds some more logos
    
    * remove others
    
    * Logos again because reasons

--- packages/js/app/src/components/TabIcon.tsx
+++ packages/js/app/src/components/TabIcon.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { 
-    View, 
-    Text
-} from 'react-native';
+import { View, Text } from 'react-native';
 
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
@@ -12,22 +9,24 @@ import colors from '../constants/colors';
 interface TabIconProps {
     name: string;
     icon: IconProp;
-    focused: boolean
+    focused: boolean;
 }
 
-const TabIcon: React.FunctionComponent<TabIconProps> = ({ name, icon, focused }) => {
+const TabIcon: React.FunctionComponent<TabIconProps> = ({
+    name,
+    icon,
+    focused,
+}) => {
     const color = focused ? colors.primary : colors.unselected;
     return (
-        <View style={{alignItems: 'center', justifyContent: 'center', top: 10} }>
-            <FontAwesomeIcon 
-                icon={icon} 
-                color={color} 
-                size={35}
-            />
+        <View
+            style={{ alignItems: 'center', justifyContent: 'center', top: 10 }}
+        >
+            <FontAwesomeIcon icon={icon} color={color} size={35} />
             <Text
                 style={{
-                    color: color, 
-                    fontSize: 12 
+                    color,
+                    fontSize: 12,
                 }}
             >
                 {name}
