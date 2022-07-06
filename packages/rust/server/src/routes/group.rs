a71840e03c9d8a2eb2b630dbbf5f3fe2cee0537e
clef98 <ericysong007@gmail.com>
2022-07-05 23:02:43 -0400

    json requests and responses in controller, implementation of functions

--- packages/rust/server/src/routes/group.rs
+++ packages/rust/server/src/routes/group.rs
@@ -2,6 +2,7 @@ use std::future::Future;
 use axum::extract::Path;
 use axum::routing::{get, post};
 use axum::handler::Handler;
+use axum::extract;
 use axum::{Json, Router};
 
 pub fn router() -> Router {
@@ -14,6 +15,18 @@ pub fn router() -> Router {
         .route("/group/:id", delete(delete_group));
 }
 
+// Accept data -> deserialiable
+// Return Data -> Serializable
+
+//create and read functions
+#[derive(Serialize)]
+pub struct GroupInfo {
+    id: i32,
+    name: String,
+    created: String,
+}
+
+//Respond with JSON: id, name, created_date
 
 async fn create(Path(params): Path<String>){
     let name: String = params.get("name");
@@ -23,38 +36,53 @@ async fn create(Path(params): Path<String>){
     todo!();
 }
 
-async fn read(Path(params): Path<String>){
-    let group_id = params.get("id");
-    todo!();
+// respond with JSON: id, name, created_date
+async fn read(Path(params): Path<i32>) -> Json<GroupInfo> {
+    let id = params.get("id");
+    let group_response = Group.read(id);
+    let new_group = GroupInfo {
+        id: group_response.id,
+        name: group_response.group_name,
+        created: group_response.created,
+    };
+    Json(new_group)
 }
 
-// unsure about multiple inputs
-async fn read_users(Path(params): Path<String>){
-    let group_id = params.get("id");
-    todo!();
+#[derive(Deserialize)]
+pub struct UsersIDs {
+    users: Vec<i32>,
 }
 
-async fn add_users(Path((id, new_user_ids)): Path<(String, Vec<String>)>){
+
+//request JSON: vec<user_ids>
+async fn add_users(Path(id): Path<i32>, extract::Json(payload): extract::Json<UsersIDs>) {
     let group_id = id.get("id");
-    let new_users = new_user_ids.get("new_users");
-    todo!();
+    let user_ids: Vec<i32> = payload.users;
+    Group.add_users(group_id, user_ids);
 }
 
-async fn delete_users(Path((id, user_ids_to_delete)): Path<(String, Vec<String>)>){
+//request JSON: vec<user_ids>
+async fn delete_users(Path(id): Path<i32>, extract::Json(payload): extract::Json<UsersIDs>) {
     let group_id = id.get("id");
     let users_to_delete = user_ids_to_delete.get("users_to_delete");
     todo!();
 }
 
-async fn change_name(Path((id, new_name)): Path<(String, Vec<String>)>, Json(payload): Json<String>){
+#[derive(Serialize)]
+pub struct NameChange {
+    name: String,
+}
+
+//request json: name
+async fn change_name(Path(id): Path<i32>, Json(payload): Json<String>) -> Json<NameChange> {
     let group_id = id.get("id");
-    let name = new_name.get("name");
     //must resolve where normal rust or json is how requests replies sent
-    let name_to_change = payload;
+    let name_to_change = payload.name;
     todo!();
 }
 
-async fn delete_group(Path(params): fn(Path<String>)){
+//none, just id passed from path
+async fn delete_group(Path(params): fn(Path<String>)) {
     let group_id = params.get("id");
     todo!();
 }
