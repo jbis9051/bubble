8910e78673453a9b0c4a0e97fcab9a8259aeb7c7
johnnyr26 <ramirezjohnny@icloud.com>
2022-07-07 22:59:20 -0400

    Create js github workflow (#11)
    * Create js github workflow
    
    * Prettify code and add build:ci command
    
    * Fix js.yml file and edit pre-commit file

--- packages/js/app/src/components/TabBar.tsx
+++ packages/js/app/src/components/TabBar.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import {
+    faHouse as homeIcon,
+    faUserGroup as friendsIcon,
+    faMap as mapIcon,
+    faCircleUser as userIcon,
+} from '@fortawesome/free-solid-svg-icons';
 import HomeScreen from '../views/HomeScreen';
 import MapScreen from '../views/MapScreen';
 import ProfileScreen from '../views/ProfileScreen';
 import FriendsScreen from '../views/FriendsScreen';
 
 import TabIcon from './TabIcon';
-import { 
-    faHouse as homeIcon,
-    faUserGroup as friendsIcon,
-    faMap as mapIcon,
-    faCircleUser as userIcon
-} from '@fortawesome/free-solid-svg-icons';
 
 const Tab = createBottomTabNavigator();
-
 
 const TabBar = () => (
     <Tab.Navigator
@@ -24,58 +23,49 @@ const TabBar = () => (
             tabBarStyle: {
                 backgroundColor: '#ffffff',
                 height: 90,
-            }
+            },
         }}
-        initialRouteName='Home'
+        initialRouteName="Home"
     >
         <Tab.Screen
-            name='Home'
+            name="Home"
             component={HomeScreen}
             options={{
                 tabBarIcon: ({ focused }) => (
-                    <TabIcon
-                        name='Home'
-                        icon={homeIcon}
-                        focused={focused}
-                    />
-                )
+                    <TabIcon name="Home" icon={homeIcon} focused={focused} />
+                ),
             }}
         />
-        <Tab.Screen name="Friends" 
+        <Tab.Screen
+            name="Friends"
             component={FriendsScreen}
             options={{
-                tabBarIcon: ({focused}) => (
+                tabBarIcon: ({ focused }) => (
                     <TabIcon
-                        name='Friends'
+                        name="Friends"
                         icon={friendsIcon}
                         focused={focused}
                     />
-                )
-            }} 
+                ),
+            }}
         />
-        <Tab.Screen name="Map" 
-            component={MapScreen} 
+        <Tab.Screen
+            name="Map"
+            component={MapScreen}
             options={{
-                tabBarIcon: ({focused}) => (
-                    <TabIcon
-                        name='Map'
-                        icon={mapIcon}
-                        focused={focused}
-                    />
-                )
-            }} 
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon name="Map" icon={mapIcon} focused={focused} />
+                ),
+            }}
         />
-        <Tab.Screen name="Profile" 
-            component={ProfileScreen} 
+        <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
             options={{
-                tabBarIcon: ({focused}) => (
-                    <TabIcon
-                        name='Profile'
-                        icon={userIcon}
-                        focused={focused}
-                    />
-                )
-            }} 
+                tabBarIcon: ({ focused }) => (
+                    <TabIcon name="Profile" icon={userIcon} focused={focused} />
+                ),
+            }}
         />
     </Tab.Navigator>
 );
