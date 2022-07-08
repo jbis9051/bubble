e2fcdd92da7ae34e66efbd50bc898fd697133bda
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-07 20:00:06 -0400

    Prettify code and add build:ci command

--- packages/js/app/src/views/FriendsScreen.tsx
+++ packages/js/app/src/views/FriendsScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-
 const FriendsScreen = () => (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Friends</Text>
