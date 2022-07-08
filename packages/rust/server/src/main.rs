d1027b6bb03ca91d6c6f01ed9a2a30c6be6671a8
clef98 <ericysong007@gmail.com>
2022-07-07 23:48:37 -0400

    solved some rust test ci errors

--- packages/rust/server/src/main.rs
+++ packages/rust/server/src/main.rs
@@ -1,6 +1,6 @@
-mod routes;
-mod models;
 mod config;
+mod models;
+mod routes;
 
 use sqlx::postgres::PgPoolOptions;
 
@@ -8,5 +8,6 @@ fn main() {
     println!("Hello, world!");
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/test").await?;
+        .connect("postgres://postgres:password@localhost/test")
+        .await?;
 }
--- packages/rust/server/src/routes/group.rs
+++ packages/rust/server/src/routes/group.rs
@@ -1,12 +1,10 @@
-use std::future::Future;
+use crate::models::group::Group;
+use axum::extract;
 use axum::extract::Path;
 use axum::routing::{get, post};
-use axum::handler::Handler;
-use axum::extract;
 use axum::{Json, Router};
-use crate::models::group;
-use crate::models::group::Group;
-
+use serde::{Serialize, Deserialize};
+use axum::routing::delete;
 
 pub fn router() -> Router {
     let app = Router::new()
@@ -32,8 +30,8 @@ pub struct GroupInfo {
 //Respond with JSON: id, name, created_date
 
 async fn create(Path(params): Path<String>) -> Json<GroupInfo> {
-    let name = params.get("name");
-    let group_response = Group.create(name);
+    let name: String = params.get("name").to_string();
+    let group_response = Group::create(name);
     let new_group = GroupInfo {
         uuid: group_response.uuid,
         name: group_response.group_name,
@@ -44,8 +42,8 @@ async fn create(Path(params): Path<String>) -> Json<GroupInfo> {
 
 // respond with JSON: id, name, created_date
 async fn read(Path(params): Path<String>) -> Json<GroupInfo> {
-    let uuid = params.get("id");
-    let group_response = Group.read(uuid);
+    let uuid = params.get("id").to_string();
+    let group_response = Group::read(uuid);
     let new_group = GroupInfo {
         uuid: group_response.uuid,
         name: group_response.group_name,
@@ -59,19 +57,18 @@ pub struct UsersIDs {
     users: Vec<i32>,
 }
 
-
 //request JSON: vec<user_ids>
 async fn add_users(Path(id): Path<String>, extract::Json(payload): extract::Json<UsersIDs>) {
-    let group_id = id.get("id");
-    let user_ids: Vec<i32> = payload.users;
-    Group.add_users(group_id, user_ids);
+    let group_id = id.get("id").to_string();
+    let user_ids: &[i32] = &*payload.users;
+    Group::add_users(group_id, user_ids);
 }
 
 //request JSON: vec<user_ids>
 async fn delete_users(Path(params): Path<String>, extract::Json(payload): extract::Json<UsersIDs>) {
-    let group_id = params.get("id");
+    let group_id = params.get("id").to_string();
     let users_to_delete = payload.users;
-    Group.delete_users(group_id, users_to_delete);
+    Group::delete_users(group_id, users_to_delete);
 }
 
 #[derive(Serialize)]
@@ -80,16 +77,18 @@ pub struct NameChange {
 }
 
 //request json: name
-async fn change_name(Path(params): Path<String>, extract::Json(payload): extract::Json<NameChange>) {
-    let group_id = params.get("id");
+async fn change_name(
+    Path(params): Path<String>,
+    extract::Json(payload): extract::Json<NameChange>,
+) {
+    let group_id = params.get("id").to_string();
     //must resolve where normal rust or json is how requests replies sent
     let name_to_change = payload.name;
-    Group.change_name(group_id, name_to_change);
+    Group::change_name(group_id, name_to_change);
 }
-
 
 //none, just id passed from path
 async fn delete_group(Path(params): Path<String>) {
-    let group_id = params.get("id");
-    Group.delete_group(group_id);
+    let group_id = params.get("id").to_string();
+    Group::delete_group(group_id);
 }
