0bd8b13557e5434aa4b8c142c2041ee8e0fae975
jbis9051 <josh9051@gmail.com>
2022-07-13 15:22:29 -0400

    Add cleanup

--- packages/rust/server/tests/helper.rs
+++ packages/rust/server/tests/helper.rs
@@ -2,7 +2,53 @@ use axum_test_helper::TestClient;
 use bubble::router;
 use bubble::types::DbPool;
 use sqlx::postgres::PgPoolOptions;
-use std::env;
+use std::future::Future;
+use std::{env, thread};
+use tokio::runtime::Runtime;
+
+#[macro_export]
+macro_rules! cleanup {
+    (|$dbarg:ident| $code: expr) => {{
+        use sqlx::postgres::PgPoolOptions;
+        use std::env;
+
+        async fn cleanup() {
+            let $dbarg = PgPoolOptions::new()
+                .max_connections(5)
+                .connect(&env::var("DB_URL").unwrap())
+                .await
+                .unwrap();
+            $code
+        }
+
+        Cleanup::new(Box::new(cleanup))
+    }};
+}
+
+type CB<F> = Box<dyn Fn() -> F>;
+
+pub struct Cleanup<F: 'static + Future + Send> {
+    cleanup: Option<CB<F>>,
+}
+
+impl<F: 'static + Future + Send> Cleanup<F> {
+    pub fn new(cleanup: CB<F>) -> Self {
+        Self {
+            cleanup: Some(cleanup),
+        }
+    }
+}
+impl<F: 'static + Future + Send> Drop for Cleanup<F> {
+    fn drop(&mut self) {
+        let cleanup = self.cleanup.take().unwrap();
+        let future = cleanup();
+        thread::spawn(|| {
+            Runtime::new().unwrap().block_on(future);
+        })
+        .join()
+        .unwrap();
+    }
+}
 
 pub async fn start_server() -> (DbPool, TestClient) {
     let pool = PgPoolOptions::new()
