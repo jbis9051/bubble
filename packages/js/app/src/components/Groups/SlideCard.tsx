9a4244a8a7accfd04f17179f51cbf6633366826a
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-21 19:55:48 -0400

    Add user icons to slide card in groups page

--- packages/js/app/src/components/Groups/SlideCard.tsx
+++ packages/js/app/src/components/Groups/SlideCard.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 import SlideCardTemplate from '../SlideCardTemplate';
+import UserIcon from './UserIcon';
+
+import styles from './styles';
 
 const SlideCard = () => (
-    <SlideCardTemplate style={{marginTop: 480}}>
-        <View style={{ height: 500 }}>
+    <SlideCardTemplate style={{marginTop: 600}}>
+        <View style={styles.userView}>
+            <UserIcon name='John' />
+            <UserIcon name='Santhosh' />
+            <UserIcon name='Kevin' />
+            <UserIcon name='Kyle' />
+            <UserIcon name='Sidney' />
+            <UserIcon name='Lia' />
         </View>
     </SlideCardTemplate>
 );
