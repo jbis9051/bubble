f1de0287a766f6b514ff0ddbf95f833ffc1df239
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-21 19:54:59 -0400

    Create stylesheet for Groups view

---
+++ packages/js/app/src/components/Groups/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    userView: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    userIcon: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 15,
+    }
+});
+
+export default styles;
