35596b2cfafd88b432985e7f62910d829d283eab
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-21 19:56:13 -0400

    Adjust line breaks of groups file

--- packages/js/app/src/views/FriendsScreen.tsx
+++ packages/js/app/src/views/FriendsScreen.tsx
@@ -6,12 +6,15 @@ import SlideCard from '../components/Groups/SlideCard';
 const FriendsScreen = () => (
     <View>
         <Map />
-        <ScrollView style={{ 
-            width:'100%', 
-            position: 'absolute',
-            top: 0,
-            bottom: 0
-        }} showsVerticalScrollIndicator={false}>
+        <ScrollView 
+            style={{ 
+                width:'100%', 
+                position: 'absolute',
+                top: 0,
+                bottom: 0
+            }}
+            showsVerticalScrollIndicator={false}
+        >
             <SlideCard />
         </ScrollView>
     </View>
