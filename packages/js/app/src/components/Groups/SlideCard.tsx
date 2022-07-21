629c9d1c95ba7f5ffbb4c83d82cd0656507f8156
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-21 19:26:54 -0400

    Create basic layout for groups page
    Include map taking up full screen and slide card

---
+++ packages/js/app/src/components/Groups/SlideCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import SlideCardTemplate from '../SlideCardTemplate';
+
+const SlideCard = () => (
+    <SlideCardTemplate style={{marginTop: 480}}>
+        <View style={{ height: 500 }}>
+        </View>
+    </SlideCardTemplate>
+);
+
+export default SlideCard;
