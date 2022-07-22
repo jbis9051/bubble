ac59afd7914aae05308c0e5987d6230fb3ab9e28
Starfish <songni@northvilleschools.net>
2022-07-22 11:26:22 -0400

    remove all lint errors and re run prettier

--- packages/js/app/src/views/SignupScreens/Signup2.tsx
+++ packages/js/app/src/views/SignupScreens/Signup2.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    TouchableOpacity,
-} from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Header from '../../components/Header';
 import TextInputBox from '../../components/TextInputBox';
