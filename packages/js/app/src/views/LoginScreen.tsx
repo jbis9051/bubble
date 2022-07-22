f102b1e0036dd236acf976396a2605694c21ad7a
Starfish <songni@northvilleschools.net>
2022-07-22 12:32:21 -0400

    add splash login and signup file

---
+++ packages/js/app/src/views/LoginScreen.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    TouchableOpacity, Dimensions,
+} from 'react-native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import TextInputBox from '../components/TextInputBox';
+import scaleComponent from '../components/scaleComponent';
+import colors from '../constants/colors';
+import LoginBackground from '../assets/LoginBackground.svg';
+
+type RootStackParamList = {
+    Login: undefined;
+    Signup1: undefined;
+    Signup2: undefined;
+    Splash: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+const scaledLoginButtonWidth = scaleComponent(300, false);
+const scaledTitleFont = scaleComponent(45, true);
+console.log(`${scaledLoginButtonWidth} scaledLoginButtonWidth`);
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.white,
+        width: '100%',
+        height: '100%',
+    },
+    titleContainer: {
+        top: '12%',
+        flex: 3,
+        justifyContent: 'center',
+    },
+    textContainer: {
+        flex: 1.6,
+        justifyContent: 'center',
+    },
+    loginContainer: {
+        flex: 1.5,
+        justifyContent: 'center',
+    },
+    noAccountContainer: {
+        flex: 1.25,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: scaledTitleFont,
+        fontWeight: '400',
+        color: colors.primary,
+    },
+    textInputDescriptors: {
+        color: colors.darkGrey,
+    },
+    login: {
+        width: scaledLoginButtonWidth,
+        height: 50,
+        margin: 7,
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.primary,
+    },
+    forgot: {
+        fontWeight: '200',
+        fontSize: 13,
+        textAlign: 'center',
+    },
+    otherLoginButtons: {
+        height: 40,
+        width: 40,
+        margin: 7,
+        borderWidth: 1,
+        borderRadius: 20,
+        padding: 10,
+    },
+    buttonText: {
+        fontSize: 14,
+        color: colors.white,
+        fontWeight: '600',
+    },
+    noAccountText: {
+        color: colors.primary,
+        fontSize: 16,
+        fontWeight: '300',
+    },
+    noAccountTextLink: {
+        color: colors.primary,
+        fontSize: 16,
+        fontWeight: '300',
+    },
+});
+
+const fetchLogin = async() => {
+    try{
+        const res = await fetch('/user/signup',{
+            method: 'POST',
+        });
+        const json = await res.json();
+        return json;
+    }catch(error){
+        console.error(error);
+    }
+    return null;
+}
+
+function Login({ route, navigation }: Props) {
+    return (
+        <View style={styles.container}>
+            <LoginBackground height={'100%'} width={'100%'} style={{position: 'absolute'}}/>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Sign In</Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <TextInputBox descriptor="Username" params={''} />
+                    <TextInputBox descriptor="Password" params={'password'} />
+                </View>
+                <View style={styles.loginContainer}>
+                    <TouchableOpacity style={styles.login}>
+                        <Text style={styles.buttonText}>Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.noAccountContainer}>
+                    <Text style={styles.noAccountText}>
+                        Don't have an account?
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Signup1')}
+                    >
+                        <Text style={styles.noAccountTextLink}>Sign up</Text>
+                    </TouchableOpacity>
+                </View>
+        </View>
+    );
+}
+export default Login;
---
+++ packages/js/app/src/views/SignupScreens/Signup2.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    ImageBackground,
+    TouchableOpacity, Dimensions,
+} from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import Header from '../../components/Header';
+import TextInputBox from '../../components/TextInputBox';
+import scaleComponent from "../../components/scaleComponent";
+import colors from '../../constants/Colors';
+import Signup2Background from '../../assets/SignUp2Background.svg'
+
+
+type RootStackParamList = {
+    Login: undefined;
+    Signup1: undefined;
+    Signup2: undefined;
+    Splash: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Signup2'>;
+
+const scaledSignupButtonWidth = scaleComponent(300, false);
+const scaledTitleFont = scaleComponent(45, true);
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        backgroundColor: colors.white,
+        width: '100%',
+        height: '100%',
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+    },
+    titleContainer: {
+        top: '7.5%',
+        flex: 2,
+        justifyContent: 'center',
+    },
+    signupContainer: {
+        flex: 4,
+        justifyContent: 'center',
+    },
+    signupButtonContainer: {
+        flex: 2,
+    },
+    title: {
+        fontSize: scaledTitleFont,
+        fontWeight: '400',
+        color: colors.primary,
+    },
+    signupButton: {
+        height: 50,
+        width: scaledSignupButtonWidth,
+        margin: 7,
+        borderRadius: 25,
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.primary,
+    },buttonText:{
+       color: colors.white,
+       fontWeight: '600',
+    },
+});
+
+const onSignUp = async() => {
+    return null;
+}
+
+
+function Signup({ route, navigation }: Props) {
+    return (
+        <View style={styles.container}>
+            <Signup2Background height={'100%'} width={'100%'} style={{position: 'absolute'}}/>
+                <Header page={'Signup1'}/>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Sign Up</Text>
+                </View>
+                <View style={styles.signupContainer}>
+                    <TextInputBox descriptor="Username" params={''} />
+                    <TextInputBox descriptor="Password" params={'password'} />
+                    <TextInputBox descriptor="Confirm Password" params={'password'} />
+                </View>
+                <View style={styles.signupButtonContainer}>
+                    <TouchableOpacity style={styles.signupButton} onPress={onSignUp()}>
+                        <Text style={styles.buttonText}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+        </View>
+    );
+}
+export default Signup;
