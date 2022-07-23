c0c9f1513bc285262d23e145c4e642a08cad79e1
Starfish <songni@northvilleschools.net>
2022-07-23 19:00:14 -0400

    run prettier and lint

--- packages/js/app/src/components/TextInputBox.tsx
+++ packages/js/app/src/components/TextInputBox.tsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TextInput, Text, Dimensions } from 'react-native';
-import scaleComponent from './scaleComponent';
+import { View, StyleSheet, TextInput, Text } from 'react-native';
 import colors from '../constants/colors';
-
-let scaledWidth = scaleComponent(300, false);
-scaledWidth += Dimensions.get('window').width * 0.025; // account for 2.5% padding so lines up with button
-const scaleDescriptor = scaleComponent(15, false);
 
 const styles = StyleSheet.create({
     container: {
-        width: scaledWidth,
         padding: '2.5%',
         alignItems: 'center',
     },
@@ -17,11 +11,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
     },
     textInput: {
+        height: 45,
         borderLeftWidth: 0,
         borderRightWidth: 0,
         borderTopWidth: 0,
         borderBottomWidth: 1,
-        height: 45,
         width: '100%',
         padding: 0,
     },
@@ -29,7 +23,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'flex-start',
-        fontSize: scaleDescriptor,
+        fontSize: 15,
         fontWeight: '300',
     },
     asterisk: {
