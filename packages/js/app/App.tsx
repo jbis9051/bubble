1a1109cb61d88f0c07927a8622a43717152790ec
Starfish <songni@northvilleschools.net>
2022-07-26 21:23:02 -0400

    revert changes in podfile for now

--- packages/js/app/App.tsx
+++ packages/js/app/App.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { createStackNavigator } from '@react-navigation/stack';
+import login from './src/views/LoginScreen';
+import signup1 from './src/views/SignupScreens/Signup1';
+import signup2 from './src/views/SignupScreens/Signup2';
+import splash from './src/views/SplashScreen';
 import TabBar from './src/components/TabBar';
+
+type RootStackParamList = {
+    Login: undefined;
+    Signup1: undefined;
+    Signup2: undefined;
+    Splash: undefined;
+};
+
+const App = () => {
+    const RootStack = createStackNavigator<RootStackParamList>();
+
+    return (
+        <NavigationContainer>
+            <RootStack.Navigator initialRouteName={'Splash'}>
+                <RootStack.Screen
+                    name="Splash"
+                    component={splash}
+                    options={{ headerShown: false, title: 'Life 256' }}
+                />
+                <RootStack.Screen
+                    name="Login"
+                    component={login}
+                    options={{ headerShown: false, title: 'Log In' }}
+                />
+                <RootStack.Screen
+                    name="Signup1"
+                    component={signup1}
+                    options={{ headerShown: false, title: 'Sign Up' }}
+                />
+                <RootStack.Screen
+                    name="Signup2"
+                    component={signup2}
+                    options={{ headerShown: false, title: 'Sign Up' }}
+                />
+            </RootStack.Navigator>
+        </NavigationContainer>
+    );
+};
 
 export default () => (
     <NavigationContainer>
