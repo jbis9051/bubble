da4d93b699f06f0f4a37ec95c768b336c5dfca51
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-26 20:58:31 -0400

    Start creating modal for Groups page

--- packages/js/app/src/views/FriendsScreen.tsx
+++ packages/js/app/src/views/FriendsScreen.tsx
@@ -6,17 +6,7 @@ import SlideCard from '../components/Groups/SlideCard';
 const FriendsScreen = () => (
     <View>
         <Map />
-        <ScrollView 
-            style={{ 
-                width:'100%', 
-                position: 'absolute',
-                top: 0,
-                bottom: 0
-            }}
-            showsVerticalScrollIndicator={false}
-        >
-            <SlideCard />
-        </ScrollView>
+        <SlideCard />
     </View>
 );
 
