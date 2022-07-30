9c3bb43ca0c4975173767b7d26e0027516bae0cf
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-30 14:43:11 -0400

    Map changes when pressing user icon
    Each user will have their own location, map will update when each user gets tapped

--- packages/js/app/src/components/Groups/UserIcon.tsx
+++ packages/js/app/src/components/Groups/UserIcon.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import ProfileImageTemplate from '../ProfileImageTemplate';
 
 import styles from './styles';
 
-const UserIcon: React.FunctionComponent<{ name: string }> = ({ name }) => (
-    <View style={styles.userIcon}>
-        <ProfileImageTemplate source="" size={80} />
-        <Text style={{ marginTop: 8, fontSize: 18 }}>{name}</Text>
-    </View>
+interface Location {
+    longitude: number;
+    latitude: number;
+}
+
+const initialRegion = {
+    longitude: -122.4324,
+    latitude: 37.78825,
+};
+
+const UserIcon: React.FunctionComponent<{
+    name: string;
+    location?: Location;
+    setLocation: (newLocation: Location) => void;
+}> = ({ name, location = initialRegion, setLocation }) => (
+    <TouchableWithoutFeedback onPress={() => setLocation(location)}>
+        <View style={styles.userIcon}>
+            <ProfileImageTemplate source="" size={80} />
+            <Text style={{ marginTop: 8, fontSize: 18 }}>{name}</Text>
+        </View>
+    </TouchableWithoutFeedback>
 );
 
 export default UserIcon;
--- packages/js/app/src/views/FriendsScreen.tsx
+++ packages/js/app/src/views/FriendsScreen.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import Map from '../components/Groups/Map';
 import SlideCard from '../components/Groups/SlideCard';
 
-const FriendsScreen = () => (
-    <View style={{ flex: 1 }}>
-        <Map />
-        <SlideCard minHeight={70} maxHeight={200} />
-    </View>
-);
+interface Location {
+    longitude: number;
+    latitude: number;
+}
+
+const initialRegion = {
+    longitude: -122.4324,
+    latitude: 37.78825,
+};
+
+const FriendsScreen = () => {
+    const [location, setLocation] = useState(initialRegion);
+    const setMapLocation = (newLocation: Location) => {
+        setLocation(newLocation);
+    };
+
+    useEffect(() => {
+        setLocation(location);
+    }, [location]);
+
+    return (
+        <View style={{ flex: 1 }}>
+            <Map location={location} />
+            <SlideCard
+                startingHeight={220}
+                minHeight={70}
+                marginTopHeight={200}
+                setLocation={setMapLocation}
+            />
+        </View>
+    );
+};
 
 export default FriendsScreen;
