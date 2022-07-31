94be9b7b284d169345a1b2cf9d0a5580b56f28cf
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-07-31 00:27:50 -0400

    Add user location pin
    The code is hella messy, hopefully will be fixed soon but gets the job done

--- packages/js/app/src/components/MapTemplate.tsx
+++ packages/js/app/src/components/MapTemplate.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Text, Platform } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import MapboxGL from '@rnmapbox/maps';
 
 MapboxGL.setAccessToken(process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string);
@@ -10,22 +10,80 @@ const initialRegion = {
     latitude: 37.78825,
 };
 
-const MapTemplate = ({ region = initialRegion, style = {} }) => {
+const MapTemplate = ({
+    region = initialRegion,
+    style = {},
+    markerRegion = initialRegion,
+    updateLocation = () => null,
+}) => {
     const [location, setLocation] = useState(region);
+    const [markerLocation, setMarkerLocation] = useState(markerRegion);
 
     useEffect(() => {
         setLocation(region);
     }, [region]);
 
+    useEffect(() => {
+        setMarkerLocation(markerRegion);
+    }, [markerRegion]);
+
     return Platform.OS === 'ios' ? (
         <MapView
             region={{
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.015,
                 ...location,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
+            }}
+            onRegionChangeComplete={(newRegion) => {
+                updateLocation(newRegion);
             }}
             style={style}
-        />
+        >
+            <Marker
+                coordinate={{
+                    latitude: markerLocation.latitude,
+                    longitude: markerLocation.longitude,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <View
+                        style={{
+                            height: 75,
+                            width: 75,
+                            backgroundColor: '#ffffff',
+                            borderRadius: 100,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            marginBottom: 5,
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontSize: 48,
+                            }}
+                        >
+                            J
+                        </Text>
+                    </View>
+                    <View
+                        style={{
+                            margin: 0,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            height: 15,
+                            width: 15,
+                            backgroundColor: '#ffffff',
+                            borderRadius: 100,
+                        }}
+                    ></View>
+                </View>
+            </Marker>
+        </MapView>
     ) : (
         <MapboxGL.MapView style={style} styleURL={MapboxGL.StyleURL.Street}>
             <MapboxGL.Camera
