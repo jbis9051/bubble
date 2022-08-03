cc488c87dceaf656496833e8a8168883d048ea87
Starfish <songni@northvilleschools.net>
2022-08-03 14:41:40 -0400

    working splash screen

--- packages/js/app/App.tsx
+++ packages/js/app/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import LoginSignup from './src/components/LoginSignup';
 
-const App = () => (
-    <NavigationContainer>
-        <LoginSignup />
-    </NavigationContainer>
-);
+const App = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 2000);
+    });
+
+    return (
+        <NavigationContainer>
+            <LoginSignup />
+        </NavigationContainer>
+    );
+};
 
 export default App;
