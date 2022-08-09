ad2a4979df5d5e32b1661ae73feb34d608be97eb
Johnny Ramirez <ramirezjohnny@icloud.com>
2022-08-09 12:22:35 -0400

    Add blur when search bar is selected

--- packages/js/app/src/components/Groups/SearchBar.tsx
+++ packages/js/app/src/components/Groups/SearchBar.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { View, TextInput, Dimensions } from 'react-native';
+import React, { useRef } from 'react';
+import { View, TextInput, Dimensions, TextBase } from 'react-native';
 import { EdgeInsets } from 'react-native-safe-area-context';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './styles';
 
-const SearchBar: React.FunctionComponent<{ insets: EdgeInsets }> = ({
-    insets,
-}) => {
+const SearchBar: React.FunctionComponent<{
+    insets: EdgeInsets;
+    isFocused: boolean;
+    setFocus: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ insets, isFocused, setFocus }) => {
     const deviceWidth = Dimensions.get('window').width;
+    const searchBar = useRef<TextInput>(null);
+
+    if (isFocused) {
+        searchBar?.current?.focus();
+    } else {
+        searchBar?.current?.blur();
+    }
+
     return (
         <View
             style={{
@@ -21,7 +31,12 @@ const SearchBar: React.FunctionComponent<{ insets: EdgeInsets }> = ({
                 icon={faMagnifyingGlass}
                 style={styles.searchIcon}
             />
-            <TextInput placeholder="Search groups" />
+            <TextInput
+                placeholder="Search groups"
+                ref={searchBar}
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
+            />
         </View>
     );
 };
--- packages/js/app/src/views/GroupsScreen.tsx
+++ packages/js/app/src/views/GroupsScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, TouchableWithoutFeedback } from 'react-native';
 import { Region } from 'react-native-maps';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { BlurView } from '@react-native-community/blur';
 import Map from '../components/Groups/Map';
 import SearchBar from '../components/Groups/SearchBar';
 import SlideCard from '../components/Groups/SlideCard';
@@ -17,13 +18,31 @@ const initialRegions: Region[] = [
 
 const GroupsScreen = () => {
     const [locations, setLocations] = useState(initialRegions);
+    const [isFocused, setFocus] = useState(false);
     const insets = useSafeAreaInsets();
 
     return (
         <View style={{ flex: 1 }}>
             <Map locations={locations} />
-            <SearchBar insets={insets} />
             <SlideCard locations={locations} setLocations={setLocations} />
+            {isFocused && (
+                <TouchableWithoutFeedback onPress={() => setFocus(false)}>
+                    <BlurView
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            right: 0,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    />
+                </TouchableWithoutFeedback>
+            )}
+            <SearchBar
+                insets={insets}
+                isFocused={isFocused}
+                setFocus={setFocus}
+            />
         </View>
     );
 };
