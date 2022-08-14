2802fd6126c83049421e2f641b43e3ee5b4f1dd0
jbis9051 <josh9051@gmail.com>
2022-08-14 17:42:36 -0400

    Bump versions add start_dev

--- packages/rust/server/src/router.rs
+++ packages/rust/server/src/router.rs
@@ -1,10 +1,25 @@
 use crate::routes;
 use crate::types::DbPool;
-use axum::{Extension, Router};
+use axum::routing::get;
+use axum::{Extension, Json, Router};
+use serde::Serialize;
 
 pub fn router(pool: DbPool) -> Router {
     Router::new()
+        .route("/", get(hello))
         .nest("/user", routes::user::router())
         .nest("/group", routes::group::router())
         .layer(Extension(pool))
+}
+
+#[derive(Serialize)]
+struct HelloInfo {
+    status: String,
+}
+
+async fn hello() -> Json<HelloInfo> {
+    (HelloInfo {
+        status: "Hello World!".to_string(),
+    })
+    .into()
 }
