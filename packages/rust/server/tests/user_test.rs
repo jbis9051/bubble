bef80592839dce331380537f549c0d09c0181c3a
jbis9051 <josh9051@gmail.com>
2023-05-09 16:56:28 -0400

    minor fixes

--- packages/rust/server/tests/user_test.rs
+++ packages/rust/server/tests/user_test.rs
@@ -311,7 +311,8 @@ async fn test_register_conflict() {
         password: "testpassword".to_string(),
         name: "testname".to_string(),
     };
-    let (_, _) = helper::initialize_user(db.pool(), &client, &created_user)
+
+    helper::initialize_user(db.pool(), &client, &created_user)
         .await
         .unwrap();
 
