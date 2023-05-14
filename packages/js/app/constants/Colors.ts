53bf1ce3f01a3d52d1415b238fced98c2cebeff6
meawa <48802197+lambdasc@users.noreply.github.com>
2023-05-13 20:26:39 -0500

    initial

---
+++ packages/js/app/constants/Colors.ts
@@ -0,0 +1,23 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+const palette: { [key: string]: string } = {
+  primary: '#02938b',
+}
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
--- packages/js/app/metro.config.js
+++ packages/js/app/metro.config.js
@@ -1,24 +1,4 @@
-const { getDefaultConfig } = require('metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-    const {
-        resolver: { sourceExts, assetExts },
-    } = await getDefaultConfig();
-    return {
-        transformer: {
-            getTransformOptions: async () => ({
-                transform: {
-                    experimentalImportSupport: false,
-                    inlineRequires: false,
-                },
-            }),
-            babelTransformerPath: require.resolve(
-                'react-native-svg-transformer'
-            ),
-        },
-        resolver: {
-            assetExts: assetExts.filter((ext) => ext !== 'svg'),
-            sourceExts: [...sourceExts, 'svg'],
-        },
-    };
-})();
+module.exports = getDefaultConfig(__dirname);
