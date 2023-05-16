663e756498adced1dd46b918758e6e79d4b24a96
JimLin21 <Jimlin@umich.edu>
2023-05-15 23:23:47 -0400

    Moved Environmental variables to Config.rs, added parameters and added uses in user.rs.

--- packages/rust/server/src/services/email.rs
+++ packages/rust/server/src/services/email.rs
@@ -1,3 +1,4 @@
+use crate::config::CONFIG;
 use sendgrid::SGClient;
 use sendgrid::SendgridError;
 use sendgrid::{Destination, Mail};
@@ -6,30 +7,29 @@ use sendgrid::{Destination, Mail};
 pub struct EmailService {}
 
 impl EmailService {
-    pub fn send(&self, _email: &str) -> Result<(), SendgridError> {
-        let mut env_vars = std::env::vars();
-        let api_key_check = env_vars.find(|var| var.0 == "SENDGRID_API_KEY"); // pull api key from env. variables
-        let sender_email = match std::env::var("sender_email") {
-            Ok(value) => value,
-            Err(_) => return Err(SendgridError::InvalidFilename),
-        };
-        let api_key: String = match api_key_check {
-            // make sure pulled value is valid
-            Some(key) => key.1,
-            None => return Err(SendgridError::InvalidFilename),
-        };
+    pub fn send(
+        &self,
+        _email: &str,
+        _subject: &str,
+        _recipient: (&str, &str),
+        _hasHTML: bool,
+        _html_content: &str,
+    ) -> Result<(), SendgridError> {
+        let api_key = &CONFIG.api_key_check; // pull api key
 
         // create mail object and add sender, recipient data
         let mail_info = Mail::new()
             .add_to(Destination {
-                address: "branh@umich.edu",
-                name: "Brandon",
+                address: _recipient.0,
+                name: _recipient.1,
             })
-            .add_from(&sender_email)
+            .add_from(&CONFIG.sender_email)
             .add_from_name("Bubble")
-            .add_subject("Test Email")
+            .add_subject(_subject)
             .add_text(_email);
-
+        if _hasHTML {
+            mail_info = mail_info.add_html(_html_content);
+        }
         // do stuff to send
         let client = SGClient::new(api_key);
         client.send(mail_info)?;
--- packages/rust/server/tests/email_service_test.rs
+++ packages/rust/server/tests/email_service_test.rs
@@ -4,9 +4,13 @@ use bubble::services::email::EmailService;
 fn email_test() {
     let email_service = EmailService::default();
 
-    let email = "Hi whats up";
+    let email = "Whats uppppp";
+    let has_html = false;
+    let subject = "TEST email";
+    let recipient = ("branh@umich.edu", "Brandon");
+    let html_content = "";
 
-    let sent = email_service.send(email);
+    let sent = email_service.send(email, subject, recipient, has_html, html_content);
 
     assert!(sent.is_ok());
 }
