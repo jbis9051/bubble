3ee576d9522af0f0444dd20743706f6d68a3be48
JimLin21 <Jimlin@umich.edu>
2023-05-18 17:12:38 -0400

    Fixes

--- packages/rust/server/src/services/email.rs
+++ packages/rust/server/src/services/email.rs
@@ -3,15 +3,26 @@ use sendgrid::SGClient;
 use sendgrid::SendgridError;
 use sendgrid::{Destination, Mail};
 
+
 #[derive(Default, Clone, Copy)]
 pub struct EmailService {}
+pub struct MockEmailService {}
 
-impl EmailService {
-    pub fn send(
+pub trait Email_ {
+    fn send(&self,
+            _email: &str,
+            _subject: &str,
+            _recipient: &Vec<(String, String)>,
+            _hasHTML: bool,
+            _html_content: &str) -> Result<(), SendgridError>;
+}
+
+impl Email_ for EmailService{
+    fn send(
         &self,
         _email: &str,
         _subject: &str,
-        _recipient: (&str, &str),
+        _recipient: &Vec<(String, String)>,
         _hasHTML: bool,
         _html_content: &str,
     ) -> Result<(), SendgridError> {
@@ -19,14 +30,17 @@ impl EmailService {
 
         // create mail object and add sender, recipient data
         let mut mail_info = Mail::new()
-            .add_to(Destination {
-                address: _recipient.0,
-                name: _recipient.1,
-            })
             .add_from(&CONFIG.sender_email)
             .add_from_name("Bubble")
             .add_subject(_subject)
             .add_text(_email);
+
+        for tuple in _recipient {
+            mail_info = mail_info.add_to(Destination {
+                address: &tuple.0,
+                name: &tuple.1,
+            })
+        }
         if _hasHTML {
             mail_info = mail_info.add_html(_html_content);
         }
@@ -37,3 +51,48 @@ impl EmailService {
         Ok(())
     }
 }
+
+impl Email_ for MockEmailService {
+    fn send(&self,
+            _email: &str,
+            _subject: &str,
+            _recipients: &Vec<(String, String)>,
+            _hasHTML: bool,
+            _html_content: &str) -> Result<(), SendgridError> {
+
+        let api_key = &CONFIG.api_key_check;
+        let mut mail_info = Mail::new()
+            .add_from(&CONFIG.sender_email)
+            .add_from_name("Bubble")
+            .add_subject(_subject)
+            .add_text(_email);
+
+        for tuple in _recipients {
+            mail_info = mail_info.add_to(Destination {
+                address: &tuple.0,
+                name: &tuple.1,
+            })
+        }
+
+        if _hasHTML {
+            mail_info = mail_info.add_html(_html_content);
+        }
+
+
+        let actual = String::from(format!("Mock Email sent to: {}, Subject: {}, Body: {}, HTML: {}", mail_info.to[0].name, mail_info.subject,
+            mail_info.text, mail_info.html));
+        let expected =  String::from(format!("Mock Email sent to: {}, Subject: {}, Body: {}, HTML: {}", _recipients[0].1, _subject,
+                                             _email, _html_content));
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+}
+
+
+
+
+
+
+
+
