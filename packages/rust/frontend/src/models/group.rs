c9dec98ff4353837db31412780453656be57ed6a
gannon_smith <gansmith@umich.edu>
2023-06-13 12:22:47 -0400

    groups structure

---
+++ packages/rust/frontend/src/models/group.rs
@@ -0,0 +1,21 @@
+use sqlx::types::Uuid;
+
+use openmls::prelude::*;
+use openmls_rust_crypto::{OpenMlsRustCrypto};
+
+
+pub async fn create_group(clients: Vec<Uuid>) -> Result<(), ()> {
+    Ok(())
+}
+
+pub async fn get_groups() -> Result<(), ()> {
+    Ok(())
+}
+
+pub async fn add_member(group_uuid: Uuid) -> Result<(), ()> {
+    Ok(())
+}
+
+pub async fn remove_member(group_uuid: Uuid) -> Result<(), ()> {
+    Ok(())
+}
--- packages/rust/frontend/src/models/mod.rs
+++ packages/rust/frontend/src/models/mod.rs
@@ -1,2 +1,3 @@
 pub mod global;
 pub mod kv;
+pub mod group;
